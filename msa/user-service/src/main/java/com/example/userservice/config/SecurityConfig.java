ddf00ba56054314136d043ad30eee908dedd5f4a
zhyunk <gimwlgus@gmail.com>
2023-11-10 05:30:19 +0900

    feat : [ user-service ] Security 접근 ip 설정
    user-service로 들어오는 모든 접근에 대해 localhost에서만 허용

--- msa/user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
+++ msa/user-service/src/main/java/com/example/userservice/config/SecurityConfig.java
@@ -2,22 +2,30 @@ package com.example.userservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.function.Supplier;
 
 import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 
 @Configuration
 public class SecurityConfig {
+    private final IpAddressMatcher ALLOWED_IP_ADDRESS = new IpAddressMatcher("127.0.0.1");
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   HandlerMappingIntrospector introspector) throws Exception {
         var mvc = new MvcRequestMatcher.Builder(introspector);
         http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -27,11 +35,7 @@ public class SecurityConfig {
 
                 .authorizeHttpRequests(config -> config
                         .requestMatchers(toH2Console()).permitAll()
-                        .requestMatchers(
-                                mvc.pattern("/health_check"),
-                                mvc.pattern("/users/**")
-                        ).permitAll()
-                        .anyRequest().authenticated());
+                        .requestMatchers(mvc.pattern("/**")).access(this::ipCheck));
 
         return http.build();
     }
@@ -39,6 +43,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private AuthorizationDecision ipCheck(Supplier<Authentication> authentication, RequestAuthorizationContext context) {
+        return new AuthorizationDecision(ALLOWED_IP_ADDRESS.matches(context.getRequest()));
     }
 
 }
