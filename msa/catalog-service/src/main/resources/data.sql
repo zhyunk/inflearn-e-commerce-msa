fd90069f47d4db7d245bd301724cf88c13f060ec
zhyunk <gimwlgus@gmail.com>
2023-11-15 00:45:17 +0900

    test : 여러개의 order 인스턴스 실행 후 주문 생성
    여러개의 order 인스턴스를 실행한 후
    하나의 user 계정으로 주문 생성 api를 호출했을 때 겪는 문제를 확인하는 테스트.
    
    결과적으로 spring에서 사용하는 round robin 알고리즘에 의해
    각 인스턴스에 순차적으로 주문이 하나씩 들어갔다.

--- msa/catalog-service/src/main/resources/data.sql
+++ msa/catalog-service/src/main/resources/data.sql
@@ -4,4 +4,6 @@ insert into catalog (product_id, product_name, stock, unit_price)
      values ('CATALOG-002', 'Seoul', 200, 5500);
 insert into catalog (product_id, product_name, stock, unit_price)
      values ('CATALOG-003', 'LA', 300, 2500);
+insert into catalog (product_id, product_name, stock, unit_price)
+values ('CATALOG-004', 'Busan', 300, 2500);
 
